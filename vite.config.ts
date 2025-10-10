03d626a89b35f29729f5722aff92f653d7538309
Neerajfullstackdeveloper <nppandey2003@gmail.com>
2025-10-10 14:12:19 +0530

    added server

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
+    allowedHosts: ["sales-manage-crm.onrender.com"], // ✅ add this line
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
